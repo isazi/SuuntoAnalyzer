e18adee1ce13cd1e695c6da6f783cdc3d5a361b8
Alessio Sclocco <alessio@sclocco.eu>
2021-09-14 17:09:52 +0200

    Add the SNR statistics.

--- suunto_analyzer/json_reader.py
+++ suunto_analyzer/json_reader.py
@@ -1,23 +1,32 @@
 import json
-import numpy
+from collections import OrderedDict
 
 
 class SuuntoJSON:
     def __init__(self):
-        self.altitude = list()
-        self.gps_altitude = list()
+        self.altitude = OrderedDict()
+        self.gps_altitude = OrderedDict()
+        self.gps_snr = OrderedDict()
 
     def load_file(self, filename: str):
         with open(filename) as file:
             temp = json.load(file)
         for sample in temp["DeviceLog"]["Samples"]:
+            timestamp = sample["TimeISO8601"]
             # Altitude
             try:
-                self.altitude.append(sample["Altitude"])
+                self.altitude[timestamp] = sample["Altitude"]
             except KeyError:
                 pass
             # GPS altitude
             try:
-                self.gps_altitude.append(sample["GPSAltitude"])
+                self.gps_altitude[timestamp] = sample["GPSAltitude"]
             except KeyError:
                 pass
+            # SNR 5 best satellites
+            try:
+                self.gps_snr[timestamp] = sample["Satellite5BestSNR"]
+            except KeyError:
+                pass
+        file.close()
+
--- suunto_analyzer/suunto_analyzer.py
+++ suunto_analyzer/suunto_analyzer.py
@@ -1,12 +1,13 @@
 import argparse
-import matplotlib.pyplot
 import json_reader
+import gps
 
 
 def command_line():
     parser = argparse.ArgumentParser()
     # Files
     parser.add_argument("-f", "--filename", type=str, required=True)
+    parser.add_argument("--snr", action="store_true")
     return parser.parse_args()
 
 
@@ -14,13 +15,8 @@ def __main__():
     arguments = command_line()
     activity = json_reader.SuuntoJSON()
     activity.load_file(arguments.filename)
-    print(len(activity.altitude))
-    matplotlib.pyplot.plot(activity.altitude)
-    matplotlib.pyplot.ylabel("Altitude")
-    matplotlib.pyplot.xticks()
-    matplotlib.pyplot.plot(activity.gps_altitude)
-    matplotlib.pyplot.savefig("./altitude.png")
-    print(len(activity.gps_altitude))
+    if arguments.snr:
+        gps.gps_snr_analysis(activity)
     return 0
 
 
