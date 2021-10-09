333698a5ea0a6c388586e45a4a958618a9f66a92
Alessio Sclocco <alessio@sclocco.eu>
2021-10-09 21:34:57 +0200

    Fixed plotting with dates.

--- suunto_analyzer/plot.py
+++ suunto_analyzer/plot.py
@@ -1,25 +1,29 @@
+import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 from json_reader import SuuntoJSON
 
 
 def altitude_plot(activity: SuuntoJSON):
+    x_altitude = [datetime.datetime.fromisoformat(i) for i in activity.altitude.keys()]
+    x_gps_altitude = [datetime.datetime.fromisoformat(i) for i in activity.gps_altitude.keys()]
     matplotlib.use("GTK3Cairo")
-    plt.plot([i for i in range(0, len(activity.altitude.keys()))], activity.altitude.values(),
-             label="Altitude (altimeter)")
-    plt.plot([i for i in range(0, len(activity.gps_altitude.keys()))], activity.gps_altitude.values(),
-             label="Altitude (GNSS)")
+    plt.plot(x_altitude, activity.altitude.values(), label="Altitude (altimeter)")
+    plt.plot(x_gps_altitude, activity.gps_altitude.values(), label="Altitude (GNSS)")
     plt.ylabel("Altitude (m)")
     plt.xlabel("Time")
     plt.xticks([])
     plt.legend()
+    plt.gcf().autofmt_xdate()
     plt.show()
 
 
 def cadence_plot(activity: SuuntoJSON):
+    x_cadence = [datetime.datetime.fromisoformat(i) for i in activity.cadence.keys()]
     matplotlib.use("GTK3Cairo")
-    plt.plot([i for i in range(0, len(activity.cadence.keys()))], activity.cadence.values(), "bo")
+    plt.plot(x_cadence, activity.cadence.values(), "bo")
     plt.ylabel("Cadence (rpm)")
     plt.xlabel("Time")
     plt.xticks([])
+    plt.gcf().autofmt_xdate()
     plt.show()
