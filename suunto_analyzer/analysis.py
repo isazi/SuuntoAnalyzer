b420e37f38eea666cfb7e0c622f86cef82e91f80
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-04-01 18:41:51 +0200

    Extracted RR filter.

--- suunto_analyzer/analysis.py
+++ suunto_analyzer/analysis.py
@@ -1,8 +1,6 @@
-import math
 import numpy
-import datetime
 from suunto_analyzer.json_reader import SuuntoJSON
-from suunto_analyzer.util import parse_seconds
+from suunto_analyzer.util import parse_seconds, rr_filter
 
 
 def gps_snr_analysis(activity: SuuntoJSON):
@@ -10,7 +8,9 @@ def gps_snr_analysis(activity: SuuntoJSON):
     if len(snr_values) >= 1:
         snr_values = numpy.array(snr_values)
         print(f"Min GNSS SNR:\t{numpy.min(snr_values)}")
-        print(f"Avg GNSS SNR:\t{numpy.average(snr_values):.1f} ±{numpy.std(snr_values):.1f}")
+        print(
+            f"Avg GNSS SNR:\t{numpy.average(snr_values):.1f} ±{numpy.std(snr_values):.1f}"
+        )
         print(f"Max GNNS SNR:\t{numpy.max(snr_values)}")
         print(f"SNR histogram:\t{numpy.histogram(snr_values)[0]}")
 
@@ -20,14 +20,18 @@ def gps_error_analysis(activity: SuuntoJSON):
     if len(ehpe_values) >= 1:
         ehpe_values = numpy.array(ehpe_values)
         print(f"Min GNSS EHPE:\t{numpy.min(ehpe_values)}")
-        print(f"Avg GNSS EHPE:\t{numpy.average(ehpe_values):.1f} ±{numpy.std(ehpe_values):.1f}")
+        print(
+            f"Avg GNSS EHPE:\t{numpy.average(ehpe_values):.1f} ±{numpy.std(ehpe_values):.1f}"
+        )
         print(f"Max GNNS EHPE:\t{numpy.max(ehpe_values)}")
         print(f"EHPE histogram:\t{numpy.histogram(ehpe_values)[0]}")
     evpe_values = [i for i in activity.evpe.values()]
     if len(evpe_values) >= 1:
         evpe_values = numpy.array(evpe_values)
         print(f"Min GNSS EVPE:\t{numpy.min(evpe_values)}")
-        print(f"Avg GNSS EVPE:\t{numpy.average(evpe_values):.1f} ±{numpy.std(evpe_values):.1f}")
+        print(
+            f"Avg GNSS EVPE:\t{numpy.average(evpe_values):.1f} ±{numpy.std(evpe_values):.1f}"
+        )
         print(f"Max GNNS EVPE:\t{numpy.max(evpe_values)}")
         print(f"EVPE histogram:\t{numpy.histogram(evpe_values)[0]}")
 
@@ -40,14 +44,22 @@ def battery_analysis(activity: SuuntoJSON):
         min_battery = numpy.min(battery_values)
         print(f"Max battery:\t{max_battery * 100.0}%")
         print(f"Min battery:\t{min_battery * 100.0}%")
-        print(f"Consumption:\ttotal = {((max_battery - min_battery) * 100.0):.1f}%\thourly = {(((max_battery - min_battery) / (activity.duration / 3600)) * 100.0):.1f}%")
+        print(
+            f"Consumption:\ttotal = {((max_battery - min_battery) * 100.0):.1f}%\thourly = {(((max_battery - min_battery) / (activity.duration / 3600)) * 100.0):.1f}%"
+        )
         if (max_battery - min_battery) > 0:
-            hours, minutes = parse_seconds(activity.duration / (max_battery - min_battery))
+            hours, minutes = parse_seconds(
+                activity.duration / (max_battery - min_battery)
+            )
             print(f"Estimated life:\t{hours}h {minutes}m")
         if (max_battery - min_battery) > 0.02:
-            hours, minutes = parse_seconds(activity.duration / ((max_battery - min_battery) + 0.01))
+            hours, minutes = parse_seconds(
+                activity.duration / ((max_battery - min_battery) + 0.01)
+            )
             print(f"\t\t{hours}h {minutes}m (estimated min)")
-            hours, minutes = parse_seconds(activity.duration / ((max_battery - min_battery) - 0.01))
+            hours, minutes = parse_seconds(
+                activity.duration / ((max_battery - min_battery) - 0.01)
+            )
             print(f"\t\t{hours}h {minutes}m (estimated max)")
 
 
@@ -56,7 +68,9 @@ def cadence_analysis(activity: SuuntoJSON):
     if len(cadence_values) >= 1:
         cadence_values = numpy.array(cadence_values)
         print(f"Min cadence:\t{numpy.min(cadence_values):.2f}")
-        print(f"Avg cadence:\t{numpy.average(cadence_values):.2f} ±{numpy.std(cadence_values):.2f}")
+        print(
+            f"Avg cadence:\t{numpy.average(cadence_values):.2f} ±{numpy.std(cadence_values):.2f}"
+        )
         print(f"Max cadence:\t{numpy.max(cadence_values):.2f}")
 
 
@@ -65,7 +79,9 @@ def temperature_analysis(activity: SuuntoJSON):
     if len(temperature_values) >= 1:
         temperature_values = numpy.array(temperature_values)
         print(f"Min temp:\t{numpy.min(temperature_values):.1f}C")
-        print(f"Avg temp:\t{numpy.average(temperature_values):.1f}C ±{numpy.std(temperature_values):.1f}C")
+        print(
+            f"Avg temp:\t{numpy.average(temperature_values):.1f}C ±{numpy.std(temperature_values):.1f}C"
+        )
         print(f"Max temp:\t{numpy.max(temperature_values):.1f}C")
 
 
@@ -80,8 +96,12 @@ def altitude_analysis(activity: SuuntoJSON):
             print(f"FusedAlti:\tEnabled")
         print(f"Ascent:\t\t{activity.ascent}")
         print(f"Descent:\t{activity.descent}")
-        print(f"Min altitude:\taltimeter = {numpy.min(altitude_values):.2f}m\tGNSS = {numpy.min(gps_altitude_values):.2f}m")
-        print(f"Max altitude:\taltimeter = {numpy.max(altitude_values):.2f}m\tGNSS = {numpy.max(gps_altitude_values):.2f}m")
+        print(
+            f"Min altitude:\taltimeter = {numpy.min(altitude_values):.2f}m\tGNSS = {numpy.min(gps_altitude_values):.2f}m"
+        )
+        print(
+            f"Max altitude:\taltimeter = {numpy.max(altitude_values):.2f}m\tGNSS = {numpy.max(gps_altitude_values):.2f}m"
+        )
 
 
 def power_analysis(activity: SuuntoJSON):
@@ -89,7 +109,9 @@ def power_analysis(activity: SuuntoJSON):
     if len(power_values) >= 1:
         power_values = numpy.array(power_values)
         print(f"Min power:\t{numpy.min(power_values):.2f}")
-        print(f"Avg power:\t{numpy.average(power_values):.2f} ±{numpy.std(power_values):.2f}")
+        print(
+            f"Avg power:\t{numpy.average(power_values):.2f} ±{numpy.std(power_values):.2f}"
+        )
         print(f"Max power:\t{numpy.max(power_values):.2f}")
 
 
@@ -101,10 +123,7 @@ def hr_analysis(activity: SuuntoJSON):
         print(f"Avg HR:\t\t{numpy.average(hr_values):.2f} ±{numpy.std(hr_values):.2f}")
         print(f"Max HR:\t\t{numpy.max(hr_values):.2f}")
     elif len(activity.rr) >= 1:
-        sigma = 2.25
-        rr_mean = numpy.average(activity.rr)
-        rr_std = numpy.std(activity.rr)
-        hr_values = [(1000 / i) * 60.0 for i in activity.rr if (math.fabs(i - rr_mean) < (sigma * rr_std))]
+        hr_values = rr_filter(activity.rr)
         print(f"Min HR:\t\t{numpy.min(hr_values):.2f}")
         print(f"Avg HR:\t\t{numpy.average(hr_values):.2f} ±{numpy.std(hr_values):.2f}")
         print(f"Max HR:\t\t{numpy.max(hr_values):.2f}")
